ad3822a9a542a0ea9890a0896d10692e44044528
jaran18 <jar.an@sap.com>
2013-11-07 19:33:39 +0800

    add

---
+++ WebContent/travelsteward/StaffReqListPage.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("travelsteward.StaffReqListPage", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf travelsteward.StaffReqListPage
+	*/ 
+	getControllerName : function() {
+		return "travelsteward.StaffReqListPage";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf travelsteward.StaffReqListPage
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
